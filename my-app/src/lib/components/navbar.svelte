527f4cb89bfd71d294908a0bfc5c41409590cd45
Rishi <72419270+roy-rishi@users.noreply.github.com>
2023-02-04 13:02:42 -0800

    Initial app with navbar

---
+++ my-app/src/lib/components/navbar.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import {page} from "$app/stores";
+	const hrefs = ["Home", "Ingredients", "About"];
+
+	function isActive(href: string) {
+		console.log($page.url.pathname, href);
+		return $page.url.pathname === "/" && href === "Home" || $page.url.pathname.slice(1) === href;
+	}
+</script>
+
+<body>
+
+	<div class="topnav">
+		{#each hrefs as href}
+			<a class={isActive(href) ? 'active' : ''} href={href === "Home" ? "/" : href}>{href}</a>
+		{/each}
+
+	</div>
+</body>
+
+<style>
+	body {
+		background-color: black;
+		font-family: Verdana, Geneva, Tahoma, sans-serif;
+	}
+
+	.topnav {
+		background-color: #333;
+		overflow: hidden;
+		border-radius: 20px;
+	}
+
+	/* Style the links inside the navigation bar */
+	.topnav a {
+		float: right;
+		color: white;
+		text-align: center;
+		padding: 14px 16px;
+		text-decoration: none;
+		font-size: 17px;
+		border-radius: 5px;
+	}
+
+	/* Change the color of links on hover */
+	.topnav a:hover {
+		background-color: white;
+		color: black;
+	}
+
+	/* Add a color to the active/current link */
+	.topnav a.active {
+		background-color: lightcoral;
+		color: white;
+	}
+</style>
---
+++ my-app/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Nav from "$lib/components/navbar.svelte";
+  import "../app.css";
+</script>
+
+<Nav />
+<slot />
