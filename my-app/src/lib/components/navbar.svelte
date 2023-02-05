c827cd9058230b83726c2ac7bc3a4abbb14d2b69
mjh316 <justinhuang316@gmail.com>
2023-02-05 06:16:31 -0800

    added animations and fixed positioning of elements

--- my-app/src/lib/components/navbar.svelte
+++ my-app/src/lib/components/navbar.svelte
@@ -7,107 +7,12 @@
 		return ($page.url.pathname === '/' && href === 'Home') || $page.url.pathname.slice(1) === href;
 	}
 
-    // images
-    import TopLeft from '$lib/assets/tidepod1.png';
-    import Img1 from '$lib/assets/img1.png';
-    import Img2 from '$lib/assets/img2.png';
+	// images
+	import TopLeft from '$lib/assets/tidepod1.png';
+	import Img1 from '$lib/assets/img1.png';
+	import Img2 from '$lib/assets/img2.png';
 
-    // classes for various elements
-    let rowClasses = "w-3/6 h-96 row-elem";
-    let headerClasses = "m-8 font-bold text-4xl p-14";
-
+	// classes for various elements
+	let rowClasses = 'w-3/6 h-96 row-elem';
+	let headerClasses = 'm-8 font-bold text-4xl p-14';
 </script>
-
-<body>
-	<div class="topnav font-bold text-4xl pt-2 pl-2 pb-2">Plateful</div>
-
-    <div class="flex flex-row">
-        <div class={rowClasses}>
-            <img class="my-6 m-auto h-full" src={Img1}>
-        </div>
-        <div class="{rowClasses} {headerClasses}">
-            <h1>The Best Homecooked Meal in Your Own Pantry</h1>
-            <p class="text-xl font-normal pt-14 leading-relaxed">
-                Plateful is the ultimate kitchen assistant that helps you make the most out 
-                of the food you already have at home. Say goodbye to food waste and hello to
-                 delicious, home-cooked meals.
-            </p>
-        </div>
-    </div>
-
-    <div class="flex flex-row accent">
-        <div class="{rowClasses} {headerClasses}">
-            <h1>What is Plateful?</h1>
-            <p class="text-xl font-normal pt-14 leading-relaxed">
-                Plateful makes meal planning and recipe creation effortless. Simply select 
-                the food items you have in your kitchen and the website will search through
-                 a database of thousands of recipes to find the perfect match. From hearty 
-                 soups and stews to delectable desserts, Plateful has something for every palate.
-            </p>
-        </div>
-        <div class={rowClasses}>
-            <img class="my-6 m-auto h-full" src={Img2}>
-        </div>
-    </div>
-
-    <div class="flex flex-row">
-        <div class={rowClasses}>
-            <img class="my-6 m-auto h-full" src={Img1}>
-        </div>
-        <div class="{rowClasses} {headerClasses}">
-            <h1>How Does Plateful Work?</h1>
-            <p class="text-xl font-normal pt-14 leading-relaxed">
-                Plateful analyzes a user's ingredients and suggest recipes that not only 
-                use up what they already have, but also make the most of their unique flavor
-                 profiles. Whether it's a busy weeknight or a special occasion, Plateful is 
-                 the perfect tool for anyone looking to simplify meal planning and elevate 
-                 their cooking game
-            </p>
-        </div>
-    </div>
-</body> 
-
-<style>
-    .row-elem {
-        text-align: center;
-        color: #CC5519;
-    }
-
-    .accent {
-        background-color: #FED384;
-    }
-
-	body {
-		background-color: #ffe5b7;
-		font-family: Verdana, Geneva, Tahoma, sans-serif;
-	}
-
-	.topnav {
-        color: #FFE5B7;
-		background-color: rgba(254, 136, 75, 1);
-		overflow: hidden;
-	}
-
-	/* Style the links inside the navigation bar */
-	.topnav a {
-		float: right;
-		color: white;
-		text-align: center;
-		padding: 14px 16px;
-		text-decoration: none;
-		font-size: 17px;
-		border-radius: 5px;
-	}
-
-	/* Change the color of links on hover */
-	.topnav a:hover {
-		background-color: white;
-		color: black;
-	}
-
-	/* Add a color to the active/current link */
-	.topnav a.active {
-		background-color: lightcoral;
-		color: white;
-	}
-</style>
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,3 +1,129 @@
 <script>
-	import About from '$lib/components/about.svelte';
+	let rowClasses = 'w-1/2 h-96 row-elem';
+	let headerClasses = 'mt-24 font-bold text-4xl';
+	import Img1 from '$lib/assets/img1.png';
+	import Img2 from '$lib/assets/img2.png';
+	import '@fontsource/frank-ruhl-libre';
+	import { onMount } from 'svelte';
+	import { fade, fly, slide } from 'svelte/transition';
+	import Typewriter, { concurrent } from 'svelte-typewriter';
+
+	let animate = false;
+
+	onMount(() => {
+		animate = true;
+	});
 </script>
+
+{#if animate}
+	<div class="pagecontainer">
+		<div class="topnav font-bold text-4xl pt-2 pl-2 pb-2 basis-full">Plateful</div>
+		<div class="flex flex-row h-screen justify-center items-center">
+			<div class={rowClasses}>
+				<img in:fly={{ duration: 1200 }} alt="Food" class="my-6 m-auto h-full" src={Img1} />
+			</div>
+			<div class="{rowClasses} {headerClasses} mb-48 flex flex-col items-center justify-center">
+				<h1
+					use:concurrent={{ interval: 40 }}
+					in:slide
+					class="text-5xl font-extrabold tracking-tight leading-snug"
+				>
+					The Best Homecooked Meals in Your Own Pantry
+				</h1>
+				<p
+					in:slide={{ delay: 300 }}
+					class="text-xl font-normal mt-14 p-6 rounded-xl w-9/12 leading-relaxed paragraph"
+				>
+					Plateful is the ultimate kitchen assistant that helps you make the most out of the food
+					you
+					<span class="underline-light-blue-300 underline underline-offset-2">already have</span> at
+					home. Say goodbye to food waste and hello to delicious, home-cooked meals.
+				</p>
+			</div>
+		</div>
+
+		<div class="flex flex-row accent h-screen items-center justify-center">
+			<div class="{rowClasses} {headerClasses} flex flex-col items-center">
+				<h1 class="text-6xl font-extrabold tracking-tight">What is Plateful?</h1>
+				<p
+					class="text-xl font-normal mt-14 p-6 w-8/12 rounded-xl text-left leading-relaxed paragraph"
+				>
+					Plateful makes meal planning and recipe creation effortless. Simply select the food items
+					you have in your kitchen and the website will search through a database of thousands of
+					recipes to find the perfect match. From hearty soups and stews to delectable desserts,
+					Plateful has something for every palate.
+				</p>
+			</div>
+			<div class={rowClasses}>
+				<img alt="chefs" class="my-6 m-auto h-full" src={Img2} />
+			</div>
+		</div>
+
+		<div class="flex flex-row h-screen items-center justify-center">
+			<div class={rowClasses}>
+				<img alt="food" class="my-6 m-auto h-full" src={Img1} />
+			</div>
+			<div class="{rowClasses} {headerClasses} flex flex-col items-center">
+				<h1 class="text-5xl font-extrabold tracking-tight">How Does Plateful Work?</h1>
+				<p
+					class="text-xl font-bold mt-14 p-4 rounded-xl pr-8 leading-relaxed paragraph text-right w-8/12"
+				>
+					Plateful analyzes a user's ingredients and suggest recipes that not only use up what they
+					already have, but also make the most of their unique flavor profiles. Whether it's a busy
+					weeknight or a special occasion, Plateful is the perfect tool for anyone looking to
+					simplify meal planning and elevate their cooking game
+				</p>
+			</div>
+		</div>
+	</div>
+{/if}
+
+<style>
+	.row-elem {
+		text-align: center;
+		color: #cc5519;
+	}
+
+	.accent {
+		background-color: #fed384;
+	}
+
+	.pagecontainer {
+		background-color: #ffe5b7;
+		font-family: 'Frank Ruhl Libre', Verdana, Geneva, Tahoma, sans-serif;
+	}
+
+	.topnav {
+		color: #ffe5b7;
+		background-color: rgba(254, 136, 75, 1);
+		overflow: hidden;
+	}
+
+	/* Style the links inside the navigation bar */
+	.topnav a {
+		float: right;
+		color: white;
+		text-align: center;
+		padding: 14px 16px;
+		text-decoration: none;
+		font-size: 17px;
+		border-radius: 5px;
+	}
+
+	/* Change the color of links on hover */
+	.topnav a:hover {
+		background-color: white;
+		color: black;
+	}
+
+	.paragraph {
+		border-width: 2px;
+		background-color: #fff5e4;
+	}
+
+	/* Add a color to the active/current link */
+	.topnav a.active {
+		background-color: lightcoral;
+		color: white;
+	}
+</style>
