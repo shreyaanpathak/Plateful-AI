1dcb6909e155245ef39384b3734ae2feedf8d800
mjh316 <justinhuang316@gmail.com>
2023-03-01 21:41:03 -0800

    vercel

--- my-app/svelte.config.js
+++ my-app/svelte.config.js
@@ -1,12 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      runtime: 'edge'
-    })
+    adapter: adapter(
+      {
+        runtime: 'edge'
+      }
+    )
   },
   preprocess: vitePreprocess()
 };
